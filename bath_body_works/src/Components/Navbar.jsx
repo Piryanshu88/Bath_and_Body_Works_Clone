693381b639d5c713e44601059aca5ab00969a6e7
Piryanshu88 <deepubisht2004@gmail.com>
2022-10-01 00:25:14 +0530

    adding single products details and search fn

--- bath_body_works/src/Components/Navbar.jsx
+++ bath_body_works/src/Components/Navbar.jsx
@@ -1,7 +1,36 @@
 import {Flex, Image,Box,ButtonGroup,Button,Spacer, HStack, Icon,Input, InputGroup,InputRightElement, Text, IconButton} from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 import { SearchIcon} from '@chakra-ui/icons'
+import React from 'react';
+
+let id;
+function debounce(fn){
+    if(id){
+        clearInterval(id)
+    }
+   id= setTimeout(function(){
+        fn()
+    },1000)
+}
+// Api call---->
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '673350cdfdmsh46e0b62adddf144p1d768bjsn3d8878c91b82',
+		'X-RapidAPI-Host': 'sephora.p.rapidapi.com'
+	}
+};
+
+const get=(query)=>{
+    return fetch(`https://sephora.p.rapidapi.com/products/search?q=${query}&pageSize=60&currentPage=1`, options).then((res)=>res.json()).then((re)=>re)
+}
+
 function Navbar(){
+    const [username, setUsername] = React.useState("");
+    console.log(username)
+    const handleClick=()=>{
+        console.log(get(username))
+    }
     return  (
         <Box>
         <Flex minWidth='max-content' alignItems='center' gap='2' border='1px solid'justifyContent='space-evenly' p="1" >
@@ -9,10 +38,10 @@ function Navbar(){
                 <Image src='https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/on/demandware.static/Sites-BathAndBodyWorks-Site/-/default/dw22126b8b/images/svg-icons/Logos-main.svg?yocs=o_s_'  alt='company_logo' width='258px' />
             <HStack marginRight='-50px' gap='3' >
                 <InputGroup>
-                    <Input placeholder='search..' border='1px solid black' type='search' borderRightRadius='0'/>
-                    <Button  aria-label='Search database' borderLeftRadius='0' className='search'  />
+                    <Input placeholder='search..' onChange={(e)=>setUsername(e.target.value)} border='1px solid black' type='search' borderRightRadius='0'/>
+                    <InputRightElement onClick={()=>debounce(handleClick)} children={<SearchIcon/>}></InputRightElement>
                 </InputGroup>
-
+                
                 <Link to='/Login'><Image src='https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/on/demandware.static/Sites-BathAndBodyWorks-Site/-/default/dw88f6c71d/images/svg-icons/UI-MyAccount.svg?yocs=o_s_' width='36px'/></Link>
                 <Link to='/Cart'><Image src='https://cdn3.iconfinder.com/data/icons/buno-black-friday-discount/32/shopping_sale_discount_bag-512.png' width="37px"/></Link>
                 
--- bath_body_works/src/Components/Top.jsx
+++ bath_body_works/src/Components/Top.jsx
@@ -1,68 +1,137 @@
-import { Button,Box,Badge, Flex, Grid, GridItem, Image, Stack } from "@chakra-ui/react"
-import {Text} from '@chakra-ui/react'
-import axios from 'axios'
-import { Link } from "react-router-dom"
-import {useState,useEffect} from 'react'
+import {
+  Button,
+  Box,
+  Badge,
+  Flex,
+  Grid,
+  GridItem,
+  SimpleGrid,
+  Image,
+  Stack,
+  Skeleton,
+} from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-const get=()=>{
-    return axios.get(' http://localhost:8080/products');
-}
-
-function Top(){
-    const [data,setData]=useState([])
-    useEffect(()=>{
-        get().then((res)=>setData(res.data))
-        console.log(data)
-    },[])
-
-    console.log(data)
-    return (
-        <Stack w='80%' m='auto'>
-        <Stack  borderBottom='1px solid ' py='3'>
-            <Text as='b' fontSize='2xl' fontFamily='TradeGothicLTW05-BoldNo\.2,Arial,sans-serif;'>Top Offers</Text>
-        </Stack>
-        <Flex flexDirection='column'>
-         <Flex background='#F16166' color='white' justifyContent='center' p='2'><Text as='b'>LIMITED TIME ONLY!</Text></Flex>   
-        <Image  src='https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dwa3427eb2/images/Fall2022/13503W_fa3_xbsk_6x1.gif?yocs=s_' w='100%' marginBottom='10'/>
-        </Flex>
-
-
-
-        <Grid templateColumns={{base:"repeat(3,25%)"}} gap='4' justifyContent='center' alignItems='center' lineHeight='2' > 
-            {data.map((el)=><Link to={`/topOffers/${el.id}`}><GridItem textAlign='center'>
-            <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-                <Box textAlign='center' >
-                <Image src={el.image_link} alt=''  m='auto'/>
-                </Box>
-      <Box p="6">
-        <Box display="flex" alignItems="baseline">
-          <Badge borderRadius="full" px="2" colorScheme="blue">
-            Top Offers for today
-          </Badge>
-          </Box>
-        <Box
-          mt="1"
-          fontWeight="semibold"
-          as="h4"
-          lineHeight="tight"
-          noOfLines={1}
+const get = () => {
+  return axios.get(" http://localhost:8080/products");
+};
+function Top() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    get().then((res) => setData(res.data));
+    console.log(data);
+  }, []);
+  console.log(data);
+  return (
+    <Stack w="80%" m="auto">
+      <Stack borderBottom="1px solid " py="3">
+        <Text
+          as="b"
+          fontSize="2xl"
+          fontFamily="TradeGothicLTW05-BoldNo\.2,Arial,sans-serif;"
         >
-          {el.name}
-        </Box>
+          Top Offers
+        </Text>
+      </Stack>
+      <Flex flexDirection="column">
+        <Flex background="#F16166" color="white" justifyContent="center" p="2">
+          <Text as="b">LIMITED TIME ONLY!</Text>
+        </Flex>
+        <Image
+          src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dwa3427eb2/images/Fall2022/13503W_fa3_xbsk_6x1.gif?yocs=s_"
+          w="100%"
+          marginBottom="10"
+        />
+      </Flex>
+      {data == "" ? (
+        <SimpleGrid
+        columns={{ base: 1, md: 2, lg: 3 }}
+          gap="4"
+          justifyContent="center"
+          alignItems="center"
+          lineHeight="2"
+        >
+          <Skeleton h="350"></Skeleton>
+          <Skeleton h="350"></Skeleton>
+          <Skeleton h="350"></Skeleton>
+          <Skeleton h="350"></Skeleton>
+          <Skeleton h="350"></Skeleton>
+          <Skeleton h="350"></Skeleton>
+          <Skeleton h="350"></Skeleton>
+          <Skeleton h="350"></Skeleton>
+          <Skeleton h="350"></Skeleton>
+          <Skeleton h="350"></Skeleton>
+          <Skeleton h="350"></Skeleton>
+          <Skeleton h="350"></Skeleton>
+          <Skeleton h="350"></Skeleton>
+          <Skeleton h="350"></Skeleton>
+          <Skeleton h="350"></Skeleton>
+          <Skeleton h="350"></Skeleton>
+          <Skeleton h="350"></Skeleton>
+          <Skeleton h="350"></Skeleton>
+        </SimpleGrid>
+      ) : (
+        <Grid
+          templateColumns={{ base: "repeat(3,25%)" }}
+          gap="4"
+          justifyContent="center"
+          alignItems="center"
+          lineHeight="2"
+        >
+          {data.map((el) => (
+            <Link to={`/topOffers/${el.id}`}>
+              <GridItem textAlign="center">
+                <Box
+                  maxW="sm"
+                  borderWidth="1px"
+                  borderRadius="lg"
+                  overflow="hidden"
+                >
+                  <Box textAlign="center">
+                    <Image src={el.image_link} alt="" m="auto" />
+                  </Box>
+                  <Box p="6">
+                    <Box display="flex" alignItems="baseline">
+                      <Badge borderRadius="full" px="2" colorScheme="blue">
+                        Top Offers for today
+                      </Badge>
+                    </Box>
+                    <Box
+                      mt="1"
+                      fontWeight="semibold"
+                      as="h4"
+                      lineHeight="tight"
+                      noOfLines={1}
+                    >
+                      {el.name}
+                    </Box>
 
-        <Box>
-          {`$${el.price}`}
-          <Box as="span" color="gray.600" fontSize="sm">
-            / 1pcs
-          </Box>
-        </Box>
-        <Button bg='blackAlpha.900' color='white' px='20' borderRadius='0'>Shop Now</Button>
-      </Box>
-    </Box>
-            </GridItem></Link>)}
+                    <Box>
+                      {`$${el.price}`}
+                      <Box as="span" color="gray.600" fontSize="sm">
+                        / 1pcs
+                      </Box>
+                    </Box>
+                    <Button
+                      bg="blackAlpha.900"
+                      color="white"
+                      px="20"
+                      borderRadius="0"
+                    >
+                      Shop Now
+                    </Button>
+                  </Box>
+                </Box>
+              </GridItem>
+            </Link>
+          ))}
         </Grid>
-        </Stack>
-    )   
+      )}{" "}
+    </Stack>
+  );
 }
 
-export default Top
+export default Top;
